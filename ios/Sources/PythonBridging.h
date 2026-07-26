0528acdbd393bdb993146803d022dd86be459160
1mrnewton <eaidoo015@gmail.com>
2026-07-26 10:22:06 +0000

    ...

--- ios/Sources/PythonBridging.h
+++
@@ -1,4 +0,0 @@
-// Bridging header: exposes the embedded CPython C API to Swift.
-// The Python.xcframework is linked as a framework, so its umbrella header is
-// imported as <Python/Python.h>.
-#import <Python/Python.h>
